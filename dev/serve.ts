641a4aefdaf8cafe8ef2306931bdf98ac9b572bc
Malkov Alexander <malkov.alexander.e@gmail.com>
2022-02-15 12:45:37 +0300

    init library

---
+++ dev/serve.ts
@@ -0,0 +1,12 @@
+import Vue, { VNode } from 'vue';
+import Dev from './serve.vue';
+// To register individual components where they are used (serve.vue) instead of using the
+// library as a whole, comment/remove this import and it's corresponding "Vue.use" call
+import TestPackage from '@/entry.esm';
+Vue.use(TestPackage);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: (h): VNode => h(Dev),
+}).$mount('#app');
---
+++ src/lib-components/index.ts
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as TestPackageSample } from './test-package-sample.vue';
