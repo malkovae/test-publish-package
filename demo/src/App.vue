fea8f2b92bf10a68b4aaa25446e9fa2ca244edaa
Malkov Alexander <malkov.alexander.e@gmail.com>
2022-03-03 17:50:21 +0300

    Test change

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Welcome friend to Your Vue.js + TypeScript App"/>
     <test-package-sample />
   </div>
 </template>
